4ad340c5c5f7943e85066ebacd36d07907499f23
Rishana125 <marinka.lagunova@gmail.com>
2021-04-11 03:02:39 +0700

    Fix config file

--- lab4/api/configs/db_connection.py
+++ lab4/api/configs/db_connection.py
@@ -1,13 +1,15 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(BASEDIR + '/db_config.env', verbose=True)
 
 SSH_HOST = os.getenv('SSH_HOST')
-SSH_PORT = os.getenv('SSH_PORT')
+SSH_PORT = int(os.getenv('SSH_PORT'))
 
 REMOTE_HOST = os.getenv('REMOTE_HOST')
-REMOTE_PORT = os.getenv('REMOTE_PORT')
+REMOTE_PORT = int(os.getenv('REMOTE_PORT'))
 
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
