c98d43957036d220e9f678333f14bb6dd99e0ea5
takaiten <lopilopj@gmail.com>
2021-04-11 15:50:35 +0700

    Add GET request for current user by token

--- lab4/api/main.py
+++ lab4/api/main.py
@@ -121,6 +121,17 @@ def get_roles(offset: int = DEFAULT_OFFSET, limit: int = DEFAULT_LIMIT,
 
 # --- USERS --- #
 
+@app.get('/users/me', response_model=schemas.User)
+def get_user_current(authorize: AuthJWT = Depends(), db: Session = Depends(get_db)):
+    current_user_id = get_current_user(authorize)
+
+    db_user = crud.get_user(db, user_id=current_user_id)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    return db_user
+
 @app.get('/users/{user_id}', response_model=schemas.User)
 def get_user(user_id: int, authorize: AuthJWT = Depends(), db: Session = Depends(get_db)):
     authorize.jwt_required()
